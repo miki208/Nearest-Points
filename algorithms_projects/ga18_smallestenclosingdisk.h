2900ab9e2bc4ef446ad837feac796d6078fe5020
libido <leonardovlibido@gmail.com>
2017-12-26 02:04:38 +0100

    add brute force algorithm, iron out bugs

--- algorithms_projects/ga18_smallestenclosingdisk.h
+++ algorithms_projects/ga18_smallestenclosingdisk.h
@@ -22,30 +22,51 @@ struct Circle
 public:
     Circle(QPointF cen, double rad, double ar)
         : center(cen), r(rad), area(ar) {}
+    Circle( const Circle& another)
+        : center(another.center), r(another.r), area(another.area) {}
+    Circle& operator=( const Circle other );
     Circle(QPointF p1, QPointF p2, QPointF p3);
     Circle(QPointF p1, QPointF p2);
+public:
     QPointF center;
     double r;
     double area;
 };
 
 
-class ga18_smallestEnclosingDisk : public AlgorithmBase
+class MinEnclosingCircle : public AlgorithmBase
 {
 public:
-    ga18_smallestEnclosingDisk(QWidget *pRenderer, int delayMs, std::string filename);
+    MinEnclosingCircle(QWidget *pRenderer, int delayMs, std::string filename);
+    MinEnclosingCircle(QWidget *pRenderer, int delayMs, std::vector<QPointF> newPoints );
 
 public:
     void runAlgorithm();
     void drawAlgorithm(QPainter &painter) const;
+
+/*  BRUTE FORCE:
+    - Theorem: For any finite set of points in
+    general position, the smallest enclosing
+    disk either has at least three points on its
+    boundary, or two points which form a
+    diameter. If there are three points, they
+    subdivide the circle into three arcs of
+    length no more than PI each. Prove !
+
+    - This implies a O(n^4) algorithm
+*/
     void runNaiveAlgorithm();
+    Circle result() const;
+    void clearResult();
 
 private:
     int n;
     std::vector<QPointF> points;
+    Circle minCircle;
     void minDisk( std::vector<QPointF> points );
     void minDisk( std::vector<QPointF> points, QPointF p1);
     void minDisk( std::vector<QPointF> points, QPointF p1, QPointF p2);
+    bool circleEnclosesPoints(Circle currCircle);
 //    Circle circleEq( QPointF p1, QPointF p2, QPointF p3);
 //    Circle circleEq( QPointF p1, QPointF p2);
 };
